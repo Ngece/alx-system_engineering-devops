b2c5649335f344412173eda8bccf141fa1e8d409
Ngece <ngecemlamli@gmail.com>
2023-03-24 07:58:43 -0700

    committing create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Create a file using Puppet
 
 file { '/tmp/school':
-    ensure => 'present',
     mode => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
+    owner => 'www-data',
+    group => 'www-data',
     content => 'I love Puppet',
 }
