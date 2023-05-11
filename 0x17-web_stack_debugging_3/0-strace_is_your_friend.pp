c4ae776d69b389829aa03def235aec576e28e156
Ngece <ngecemlamli@gmail.com>
2023-05-10 18:20:07 -0700

    committing again

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-"""  Fix wordpress issue on Apache web server and then automate it using Puppet
-"""
+# Fix wordpress issue on Apache web server and then automate it using Puppet
+
 exec { 'fix-wordpress':
   command => "/bin/sed -i /var/www/html/wp-settings.php \
   -e 's/class-wp-locale.phpp/class-wp-locale.php/'"
