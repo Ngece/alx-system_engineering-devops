3767536284b0bfec12da39b4122c2d86e4895814
Ngece <ngecemlamli@gmail.com>
2023-05-10 17:42:31 -0700

    committing done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+"""  Fix wordpress issue on Apache web server and then automate it using Puppet
+"""
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
